37ad8cba2e257473186258085abeb029d23ca47c
Nicholas Hobbs <nick@nhobb.com>
2023-06-24 22:05:45 +1000

    Single file cause hard

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,47 @@
-export * from './get-input.js';
-export * from './get-key.js';
-export * from './get-vim-cmd.js';
+import * as readline from 'node:readline';
+
+type KeyPressEvent = {sequence: string, name: string, ctrl: boolean, meta: boolean, shift: boolean};
+
+export async function getKey(): Promise<KeyPressEvent> {
+  const { stdin , stdout} = await import('node:process');
+  const rl = readline.promises.createInterface({ input: stdin, output: stdout, terminal: false });
+  stdin.setRawMode(true);
+  readline.emitKeypressEvents(stdin, rl);
+
+  let listener: (...args: any[]) => void;
+
+  return new Promise(resolve => {
+    listener = (k, e) => {
+      rl.close();
+      stdin.removeListener('keypress', listener);
+      resolve(e);
+    }
+    stdin.on('keypress', listener);
+  });
+}
+
+export async function getInput(prompt = ''): Promise<string> {
+    const { stdin , stdout} = await import('node:process');
+    const rl = readline.promises.createInterface({ input: stdin, output: stdout, terminal: true });
+    const result = await rl.question(prompt);
+    rl.close();
+    return result;
+}
+
+type VimCommand = {key: string, amount: number}
+
+export async function getVimCommand (): Promise<VimCommand> {
+  let command: Partial<VimCommand> = {key: '', amount: undefined};
+
+  while (true) {
+      command.key = (await getKey()).sequence;
+      let num = parseInt(command.key);
+      if (Number.isNaN(num)) {
+          return {
+            key: command.key,
+            amount: command.amount ?? 1
+          };
+      }
+      command.amount = (command.amount ?? 0) * 10 + num;
+  }
+}
